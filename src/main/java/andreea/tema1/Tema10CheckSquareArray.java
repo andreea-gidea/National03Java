d7d45d56a1a3d805e7e2f818a1f7ec8b0643e36d
andreea-gidea <gideaandreea@yahoo.com>
2020-11-25 00:42:02 +0200

    Commit homework exercises 10,11

---
+++ src/main/java/andreea/tema1/Tema10CheckSquareArray.java
@@ -0,0 +1,39 @@
+package andreea.tema1;
+
+public class Tema10CheckSquareArray {
+    public static void main(String[] args) {
+
+        int arraySquare[][]={{1, 2, 3},{4, 5, 6},{7, 8, 9}};
+        int arrayNotSquare[][]={{1, 3},{4, 5, 6},{7, 8, 9}};
+        if (isSquare(arraySquare)==true)
+            System.out.println("Array is square");
+        if (isSquare(arrayNotSquare)==true)
+            System.out.println("Array is square");
+        else
+            System.out.println("Array not square");
+
+
+    }
+
+    public static boolean isSquare(int[][] array) {
+        int firstLevelElements=0;
+        boolean square=true;
+        for (int i=0;i<array.length;i++){
+            firstLevelElements=firstLevelElements+1;
+            int secondLevelElements=0;
+            for (int j=0;j<array[i].length;j++){
+                secondLevelElements=secondLevelElements+1;
+            }
+            if(array.length!=secondLevelElements){
+                square=false;
+            }
+        }
+        if (square==true){
+           // System.out.println("The array is square");
+            return square;
+        }
+        else
+            //System.out.println("The array is not square");
+            return square;
+    }
+}
---
+++ src/main/java/andreea/tema1/Tema11ZileLuna.java
@@ -0,0 +1,27 @@
+package andreea.tema1;
+
+import java.util.Scanner;
+
+public class Tema11ZileLuna {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Insert the index of the month");
+        String index=sc.next();
+        if (index.equals(String.valueOf(1))|index.equals(String.valueOf(3))|index.equals(String.valueOf(5))|index.equals(String.valueOf(7))|index.equals(String.valueOf(8))|index.equals(String.valueOf(10))|index.equals(String.valueOf(12))){
+            System.out.println("The number of days in this month is equal to 31");
+        }
+        else if (index.equals(String.valueOf(2))) {
+            System.out.println("What year");
+            int year=sc.nextInt();
+            if (year%4==0)
+                System.out.println("The number of days is 29");
+            else
+                System.out.println("the number of days is 28");
+        }
+        else if (index.equals(String.valueOf(4))|index.equals(String.valueOf(6))|index.equals(String.valueOf(9))|index.equals(String.valueOf(11))){
+            System.out.println("The number of days in this month is equal to 30");
+        }
+        else
+            System.out.println("Invalid number of month");
+    }
+}
